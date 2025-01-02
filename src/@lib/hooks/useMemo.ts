2726ca64eb18b2f423e507cc2a56d9d19ffa4f27
pitang <wpeffect@gmail.com>
2025-01-02 21:34:00 +0900

    feat: useMemo 작성

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,27 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
+// useMemo 훅은 계산 비용이 높은 값을 메모이제이션합니다.
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
   // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  // 1. 이전 의존성과 결과를 저장할 ref 생성
+  const ref = useRef<{ deps: DependencyList | null; value: T | null }>({
+    deps: null,
+    value: null,
+  });
+
+  // 2. 현재 의존성과 이전 의존성 비교
+  if (ref.current?.deps === null || !_equals(ref.current?.deps, _deps)) {
+    // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+    ref.current.value = factory();
+    ref.current.deps = _deps;
+  }
+
+  // 4. 메모이제이션된 값 반환
+  return ref.current?.value as T;
 }
