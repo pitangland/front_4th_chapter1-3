99276416e65336df839c3676c9b7024ba7b4915e
pitang <wpeffect@gmail.com>
2025-01-02 21:34:12 +0900

    feat: useCallback 작성

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
+import { useMemo } from "./useMemo";
 
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  // 함수와 의존성 배열을 인자로 받고, 반환 값을 메모이제이션된 함수
+  return useMemo(() => factory, _deps);
 }
